b122fd3e7a054917e43bd0c42585cbe22185c6b2
ilyass mar <ilyass.marghine@edu.uiz.ac.ma>
2024-09-02 21:38:39 +0100

    i added a the km/h to m/s challenge

--- Desktop/SAS/kilometr_to_yards.c
+++ Desktop/SAS/kilometr_to_yards.c
@@ -9,6 +9,8 @@ scanf("%f", &disK);
 disY = disK * 1093.61;
 printf("the in yards is: %f", disY);
 
-
+//this is just a comment to test the vs code terminal 
+int myNum= 15;
+printf("hello this is the lap %d",myNum);
     return 0;
 }
